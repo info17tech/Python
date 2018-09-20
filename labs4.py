3be576130e89632b69458f4b7184d860954e534a
info17tech <43031995+info17tech@users.noreply.github.com>
2018-09-20 16:21:54 +0530

    Add files via upload

---
+++ labs4.py
@@ -0,0 +1,15 @@
+a=int(input("enter value of a"))
+if a>=90:
+	print("A+")
+elif a>=80:
+    print("A")
+elif a>=70:
+    print("B+")
+elif a>=60:
+    print("B")
+elif a>=50:
+    print("C+")
+elif a>=40:
+    print("C")
+else:
+	print("fail")
---
+++ lbs3.py
@@ -0,0 +1,5 @@
+a=int(input("enter value of a"))
+if a%2==0:
+    print("a is an even number")
+else:
+    print("a is an odd number")
