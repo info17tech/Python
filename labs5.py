5596f9d9dcaf1be4723026dcf80ffa7f7ff22caa
info17tech <43031995+info17tech@users.noreply.github.com>
2018-09-22 08:52:15 +0530

    Add files via upload

---
+++ labs5.py
@@ -0,0 +1,9 @@
+a=int(input("value of a"))
+b=int(input("value of b"))
+c=int(input("value of c"))
+if ((a>b) and (a>c)):
+     print("a is greater")
+elif(b>c):
+      print("b is greater")
+else:
+      print("c is greater")
