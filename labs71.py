b35b2947fde281f1a95def7ae141ba43caa51944
info17tech <43031995+info17tech@users.noreply.github.com>
2018-09-29 09:10:57 +0530

    Add files via upload

---
+++ labs71.py
@@ -0,0 +1,12 @@
+def add(a,b):
+	return a+b
+def sub(a,b):
+	return a-b
+
+i=int(input("enter number A:"))
+j=int(input("enter number B:"))
+
+d=add(i,j)
+print("addition is:",d)
+
+print("subtraction is:",sub(i,j))
