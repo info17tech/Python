b89e4e054fe9b7f7300a31619d06c0cd8160fa7f
info17tech <43031995+info17tech@users.noreply.github.com>
2018-09-29 08:58:03 +0530

    Add files via upload

---
+++ labs7.py
@@ -0,0 +1,35 @@
+def add():
+	a=10
+	b=20
+	c=a+b
+	print(c)
+add()
+
+def add():
+	a=10
+	b=20
+	c=a+b
+	return c
+d=add()
+print(d)
+
+def substract():
+	a=10
+	b=20
+	c=a-b
+	print(c)
+substract()
+
+def multiply():
+	a=10
+	b=20
+	c=a*b
+	print(c)
+multiply()
+
+def mod():
+	a=10
+	b=20
+	c=a%b
+	print(c)
+mod()
