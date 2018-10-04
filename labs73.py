38101193f16b600600e345ead64e9083ccd33773
info17tech <43031995+info17tech@users.noreply.github.com>
2018-10-04 15:12:15 +0530

    Add files via upload

--- labs73.py
+++ labs73.py
@@ -1,9 +1,13 @@
 import random
+#using dictionary
 a={1:'rock',2:'paper',3:'scissor'}
 while True:
 	b=input("enter your choice rock/paper/scissor:")
+	#to generate random choice
 	c=a[random.randint(1,3)]
+	#printing the choices
 	print("you chose:",b,"computer chose:",c)
+	#comparing the conditions
 	if(b==c):
 		print("draw")
 	elif((b=='rock') and (c=='paper')):
