78281f9bfc36d264ea829e860a33dabda47555c4
info17tech <43031995+info17tech@users.noreply.github.com>
2018-10-04 16:12:06 +0530

    Add files via upload

---
+++ labs8.py
@@ -0,0 +1,9 @@
+n=int(input("enter the value of n:"))
+for i in range(n,n+1):
+	for j in range(1,11):
+		print(i,"X",j,"=",i*j)
+
+
+for i in range(2,3):
+	for j in range(1,11):
+		print(i,"X",j,"=",i*j)
