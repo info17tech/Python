e4e3e01980ed7341a79352d3917f64c6282ec725
info17tech <43031995+info17tech@users.noreply.github.com>
2018-10-11 15:15:52 +0530

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,8 @@
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login("priyankarmakar17@gmail.com","*Prim@kar710")
+msg = "hello"
+server.sendmail("priyankarmakar17@gmail.com","poojitha202@gmail.com",msg)
+print('mail is sent^.^')
+server.quit()
