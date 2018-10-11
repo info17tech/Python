3eec0b29d883be39a914d84d25c7c023acef7898
info17tech <43031995+info17tech@users.noreply.github.com>
2018-10-11 15:18:08 +0530

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -1,7 +1,7 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-server.login("priyankarmakar17@gmail.com","*Prim@kar710")
+server.login("priyankarmakar17@gmail.com","********")
 msg = "hello"
 server.sendmail("priyankarmakar17@gmail.com","poojitha202@gmail.com",msg)
 print('mail is sent^.^')
