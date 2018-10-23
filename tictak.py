06e10bd12b6a51abdc4dcc266ef9f2ace663f5ad
info17tech <43031995+info17tech@users.noreply.github.com>
2018-10-23 20:35:58 +0530

    Update tictak.py

--- tictak.py
+++ tictak.py
@@ -4,63 +4,82 @@ from os import system
 
 a=[1,2,3,4,5,6,7,8,9]
 def printboard():
-	print("-----------")
-	print(a[0],'|',a[1],'|',a[2],'|')
-	print("-----------")
-	print(a[3],'|',a[4],'|',a[5],'|')
-	print("-----------")
-	print(a[6],'|',a[7],'|',a[8],'|')
+	system('clear')
+	print(a[0],'|',a[1],'|',a[2])
+	print("---------")
+	print(a[3],'|',a[4],'|',a[5])
+	print("---------")
+	print(a[6],'|',a[7],'|',a[8])
+	print("---------")
+
 def possibilities():
-	for 1 in a:
-		if(i!='x' and i!='0'):
-			1.append(i)
-	return 1
-pli=True
+	l = []
+	for i in a:
+		if (i!='X' and i != 'O'):
+			l.append(i)
+	return l
+
+def compTurn():
+	temp = possibilities()
+	k = random.choice(temp)
+	return k
+
+pl1=True
 while True:
+
 	printboard()
 	if pl1:
-		p=int(input("choose your place"))
+		p=int(input("Choose your place"))
 		if(p in a):
-			p(int("player 1 chose",p))
-			a[p-1]='k'
-			pl1= not pl1
+			print("Player 1 chose",p)
+			a[p-1]='X'
+			pl1 = not pl1
 	else:
-		w=compturn()
-		print("computer chose",w)
+		w = compTurn()
+		print("Computer chose",w)
 		sleep(3)
 		a[int(w)-1]='O'
-		pl1= not pl1
+		pl1 = not pl1
 
 	for i in (0,3,6):
 		if(a[i]==a[i+1] and a[i]==a[i+2]):
 			printboard()
-			if(a[i]=='x'):
-				print("player 1 wins")
+			if(a[i]=='X'):
+				print("Player 1 wins")
 				exit()
 			else:
-				print("computer wins")
+				print("Computer wins")
 				exit()
 
+	for i in range(3):
+		if(a[i]==a[i+3] and a[i]==a[i+6]):
+			printboard()
+			if(a[i]=='X'):
+					print("Player 1 wins")
+				exit()
+			else:
+					print("Computer wins”)
+ 					exit()
 		if(a[0]==a[4] and a[0]==a[8]):
 			printboard()
 			if(a[0]=='X'):
-				print("player 1 wins")
+					print("Player 1 wins")
 				exit()
 			else:
-				print("computer wins")
-				exit()
+					print("Computer wins”)
+ 					exit()
 		elif(a[2]==a[4] and a[2]==a[6]):
 			printboard()
 			if(a[2]=='X'):
-				print("player 1 wins")
+					print("Player 1 wins")
 				exit()
 			else:
-				print("computer wins")
-				exit()
-		t=possibilities()
-		if(len(t)==0):
+					print("Computer wins”)
+ 					exit()
+		t = possibilities()
+		if len(t)==0:
 			printboard()
-			print("it is a tie...")
+			print(“it’s a tie…”)
 			exit()
 
 
